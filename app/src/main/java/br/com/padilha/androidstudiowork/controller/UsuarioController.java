efe5ac2e763b8ec408c8525071f9308576294e8b
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2021-10-12 15:58:00 -0300

    Criação dos packages controller e dataSource; Criação da classe UsuarioController e AppDataBase;

---
+++ app/src/main/java/br/com/padilha/androidstudiowork/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package br.com.padilha.androidstudiowork.controller;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import br.com.padilha.androidstudiowork.dataModel.UsuarioDataModel;
+import br.com.padilha.androidstudiowork.dataSource.AppDataBase;
+import br.com.padilha.androidstudiowork.model.Usuario;
+
+public class UsuarioController extends AppDataBase {
+
+    ContentValues dadoDoObjeto;
+
+    public UsuarioController(Context context) {
+        super(context);
+    }
+
+    public boolean insert(Usuario obj) {
+        dadoDoObjeto = new ContentValues();
+        dadoDoObjeto.put(UsuarioDataModel.ID, obj.getId());
+        dadoDoObjeto.put(UsuarioDataModel.NOME, obj.getNome());
+        dadoDoObjeto.put(UsuarioDataModel.IDADE, obj.getIdade());
+        dadoDoObjeto.put(UsuarioDataModel.EMAIL, obj.getEmail());
+        dadoDoObjeto.put(UsuarioDataModel.PHONE, obj.getPhone());
+        dadoDoObjeto.put(UsuarioDataModel.SENHA, obj.getSenha());
+        return insert(UsuarioDataModel.TABELA,dadoDoObjeto);
+    }
+}
---
+++ app/src/main/java/br/com/padilha/androidstudiowork/dataSource/AppDataBase.java
@@ -0,0 +1,43 @@
+package br.com.padilha.androidstudiowork.dataSource;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import br.com.padilha.androidstudiowork.dataModel.UsuarioDataModel;
+
+public class AppDataBase extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "DB_Cadastros.sqlite";
+    public static final int DB_VERSION = 1;
+    SQLiteDatabase db;
+
+    public AppDataBase(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        db = getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(UsuarioDataModel.createTable());
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public boolean insert(String tabela, ContentValues dados) {
+        db = getWritableDatabase();
+        boolean retorno = false;
+        try {
+            retorno = db.insert(tabela, null, dados) > 0;
+            return retorno;
+    } catch (Exception e) {
+        }
+        return retorno;
+
+    }
+
+}
