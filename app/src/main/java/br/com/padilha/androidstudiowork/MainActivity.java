25ce2cac3389e29ac16ad9ce34bccbe2592e071d
FDAPADILHA <filipe.amaral.padilha@gmail.com>
2021-10-12 14:46:48 -0300

    Criação da nova Activity e adicionada a ação e mudar de tela;

--- app/src/main/java/br/com/padilha/androidstudiowork/MainActivity.java
+++ app/src/main/java/br/com/padilha/androidstudiowork/MainActivity.java
@@ -2,13 +2,35 @@ package br.com.padilha.androidstudiowork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button butTextLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        setButtonClick();
     }
+
+    private void initView(){
+        butTextLogin = findViewById(R.id.but_text_login);
+    }
+
+    private void setButtonClick(){
+        butTextLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                    startActivity(intent);
+            }
+        });
+    }
+
 }
